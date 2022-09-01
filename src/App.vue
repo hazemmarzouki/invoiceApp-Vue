dec60257d84c13b30bf6cdbe561497c6692cac15
Hazem Marzouki <m.hazem1812@myu.universitecentrale.tn>
2022-09-01 18:12:39 +0200

    edit navigation sidebar styling

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,49 @@
 <template>
-  <router-view />
+  <div>
+    <div v-if="!mobile" class="app flex">
+      <Navigation />
+
+      <div class="app-content flex flex-column">
+        <router-view />
+      </div>
+    </div>
+    <div v-else class="mobile-message flex-column flex">
+      <h2>Sorry , This app is not supported on Mobile Devices , </h2>
+      <p>To use This app , please use a Desktop or Tablet</p>
+      </div>
+  </div>
 </template>
+
+<script>
+import Navigation from "./components/Navigation.vue";
+
+export default {
+  data() {
+    return {
+      mobile: null,
+    };
+  },
+
+  components: {
+    Navigation,
+  },
+  created() {
+    this.checkScreen();
+    window.addEventListener("resize", this.checkScreen);
+  },
+  methods: {
+    checkScreen() {
+      const windowWidth = window.innerWidth;
+      if (windowWidth <= 600) {
+        this.mobile = true;
+        return;
+      }
+        this.mobile = false;
+      
+    },
+  },
+};
+</script>
 
 <style lang="scss">
 @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap");
@@ -10,9 +53,31 @@
   padding: 0;
   box-sizing: border-box;
   font-family: "Poppins", sans-serif;
-  background-color: #141625;
 }
 
+.app {
+  background-color: #1a1a1a;
+  min-height: 100vh;
+  flex-direction: column;
+  @media (min-width: 900px) {
+    flex-direction: row;
+  }
+
+  .app-content {
+    padding: 0 20px;
+    flex: 1;
+    position: relative;
+  }
+}
+.mobile-message {
+text-align: center;
+justify-content: center;
+align-items: center;
+height: 100vh;
+background-color: #1a1a1a;
+color: #fff;
+
+}
 button,
 .button {
   cursor: pointer;
---
+++ src/components/Navigation.vue
@@ -0,0 +1,41 @@
+<template>
+  <header class="flex">
+    <div class="branding flex">
+      <img src="../assets/file-invoice.png" alt="" />
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Navigation",
+};
+</script>
+
+<style lang="scss" scoped>
+header {
+  z-index: 99;
+  flex-direction: row;
+  background-color: #363636;
+  @media (min-width: 900px) {
+    flex-direction: column;
+    min-width: 90px;
+    min-height: 100%;
+    border-radius: 0 20px 20px 0;
+  }
+}
+.branding {
+  border-radius: 0 20px 20px 0;
+  background-color: #57acdc;
+  justify-content: center;
+  padding: 24px;
+  @media (min-width: 900px) {
+    width: 100%;
+  }
+
+  img {
+    width: auto;
+    height: 30px;
+  }
+}
+</style>
